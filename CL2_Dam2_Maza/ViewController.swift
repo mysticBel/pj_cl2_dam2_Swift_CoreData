3d60a22ffa8867802b20409e6d3b0c27e8208fde
mysticBel <dnxbel@gmail.com>
2023-10-13 20:19:40 -0500

    Initial Commit

---
+++ CL2_Dam2_Maza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CL2_Dam2_Maza
+//
+//  Created by Maribel on 13/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
